05f462daf682ebf2849da255ce1c9b919791313a
Liam Griffiths <liamgriffiths@gmail.com>
2015-06-07 21:25:59 -0400

    add touch events

--- client/client.js
+++ client/client.js
@@ -4,5 +4,8 @@ import "babel/polyfill"
 import React from "react"
 import App from "../components/app"
 
+
+React.initializeTouchEvents(true)
+
 const body = document.querySelector("body")
 React.render(<App data={ window.DATA } />, body)
