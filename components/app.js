6215f61ab15bdd0b4e7b2909ca9faf1dea420607
Liam Griffiths <liamgriffiths@gmail.com>
2015-06-07 23:18:03 -0400

    use state, not props

--- components/app.js
+++ components/app.js
@@ -44,7 +44,7 @@ export default class App extends Component {
   }
 
   render() {
-    const { statuses } = this.props.data
+    const { statuses } = this.state
     return <StatusList statuses={ statuses } />
   }
 }
