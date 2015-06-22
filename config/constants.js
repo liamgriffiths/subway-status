9814fa8fb8a34dc7f2a4beba6d0bb531eeaff59d
Liam Griffiths <liamgriffiths@gmail.com>
2015-06-22 12:57:14 -0400

    add hashmark ad asset util to load versioned assets

--- config/constants.js
+++ config/constants.js
@@ -1,7 +1,9 @@
 export const ASSETS = {
-  JS: [ "/static/bundle.js" ],
-  CSS: [ "/static/bundle.css" ]
+  JS: [ "static/bundle.js" ],
+  CSS: [ "static/bundle.css" ]
 }
+
+export const ASSET_MAP_FILE = "../static/assets.json"
 
 export const ROUTES = {
   WEB: {
@@ -12,3 +14,5 @@ export const ROUTES = {
     STATUSES: "/api/statuses"
   }
 }
+
+export const IS_PRODUCTION = process.env.NODE_ENV === "production"
